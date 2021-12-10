d088f52c27d50de209462e7cc0f8dc9308eb211c
Harut Safaryan <safaryanharut@gmail.com>
2021-12-10 16:31:10 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input text: ");
+            string text = Console.ReadLine();
+
+            var result = text.Select(x => x)
+                        .Where(x => Char.IsUpper(x))
+                        .Count();
+
+            Console.WriteLine($"The number of words is: {result}");
+            Console.ReadLine();
+        }
+    }
+}
